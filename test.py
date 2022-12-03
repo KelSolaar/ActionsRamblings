9a7855693eb82b6fb1a08ff961042ddaf12b0d27
Thomas Mansencal <thomas.mansencal@gmail.com>
2022-12-03 17:36:03 +1300

    Update test.py

--- test.py
+++ test.py
@@ -4,4 +4,4 @@ import subprocess
 print(os.getenv('PATH'))
 path = "D:/a/ActionsRamblings/ActionsRamblings/IMG_2600.CR2"
 print(os.path.exists(path))
-subprocess.check_output(["Adobe DNG Converter", path])
+subprocess.check_output(["Adobe DNG Converter", "-c", path])
